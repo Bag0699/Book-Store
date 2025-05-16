4045449705d704c3edcb057e62600e33d2e1210d
Brayam Ayala <b.ayalaga06@gmail.com>
2025-05-16 18:02:26 -0500

    creación de las entidades.

---
+++ src/main/java/com/bag/Book_Store/model/entity/Store.java
@@ -0,0 +1,15 @@
+package com.bag.Book_Store.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Store {
+    private String name;
+    private String address;
+    private String attention;
+    private String map;
+}
