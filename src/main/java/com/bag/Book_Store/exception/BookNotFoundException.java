987f6dbdd75c18dcb0aa4ae51f8b9725ffe3ce3b
Brayam Ayala <b.ayalaga06@gmail.com>
2025-05-26 16:48:47 -0500

    Se crea las excepciones personalizadas para las entidades.

---
+++ src/main/java/com/bag/Book_Store/exception/BookNotFoundException.java
@@ -0,0 +1,4 @@
+package com.bag.Book_Store.exception;
+
+public class BookNotFoundException extends RuntimeException{
+}
---
+++ src/main/java/com/bag/Book_Store/exception/OrderNotFoundException.java
@@ -0,0 +1,4 @@
+package com.bag.Book_Store.exception;
+
+public class OrderNotFoundException extends RuntimeException{
+}
