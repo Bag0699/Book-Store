89a8d35fd2a228becb6a0757ae1429f4813e4a86
Brayam Ayala Laptop <b.ayalaga06@gmail.com>
2025-06-04 14:01:07 -0500

    Corrección de typo en `CreateFormarRequest` a `CreateFormatRequest` en DTOs, servicio y mapeador.

--- src/main/java/com/bag/Book_Store/service/FormatService.java
+++ src/main/java/com/bag/Book_Store/service/FormatService.java
@@ -1,16 +1,16 @@
 package com.bag.Book_Store.service;
 
-import com.bag.Book_Store.model.dto.request.CreateFormarRequest;
+import com.bag.Book_Store.model.dto.request.CreateFormatRequest;
 import com.bag.Book_Store.model.dto.response.FormatResponse;
 
 import java.util.List;
 
 public interface FormatService {
 
-    FormatResponse save(CreateFormarRequest request);
+    FormatResponse save(CreateFormatRequest request);
     List<FormatResponse> findAll();
     FormatResponse findById(Long id);
-    FormatResponse update(Long id, CreateFormarRequest request);
+    FormatResponse update(Long id, CreateFormatRequest request);
     void deleteById(Long id);
 
 }
